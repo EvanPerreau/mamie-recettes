7aa116213565243d11fe12672947c0225adca487
EvanPerreau <evan.perreau@gmail.com>
2024-07-10 13:25:56 +0200

    Fix - fix base layout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          {children}
-        </main>
+        {children}
       </body>
     </html>
   );
